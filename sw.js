c0ef53cdb178228dd882d74d6bfcdddc1b9b1dd1
prospeccaocva-lab <prospeccaocva@gmail.com>
2026-09-23 14:11:42 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Service worker: permite instalar no celular e abrir o app sem internet
+const CACHE = 'vendas-v1';
+const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
